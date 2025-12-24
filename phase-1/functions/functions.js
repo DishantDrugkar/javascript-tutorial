09729c9a3ed71a1a5a008517cbe6b902844a1014
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-12-24 13:19:56 +0530

    Functions in javascript

---
+++ phase-1/functions/functions.js
@@ -0,0 +1,14 @@
+function print(){
+    console.log("Hello");
+    
+}
+
+print();
+
+function animal(name){
+    console.log(`${name} naach raha hai`);
+    
+}
+
+animal("horse");
+animal("Hiran");
